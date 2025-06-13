1a9c6f07b4ec1ad85fb8fc7c345db80dfabf0465
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-06-13 17:11:16 +0300

    fix: add teacher filters

---
+++ src/main/java/gr/aueb/cf/schoolapp/core/filters/TeacherFilters.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.schoolapp.core.filters;
+
+
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TeacherFilters extends GenericFilters {
+    @Nullable
+    private String uuid;
+    @Nullable
+    private String userAfm;
+    @Nullable
+    private String userAmka;
+    @Nullable
+    private Boolean isActive;
+}
